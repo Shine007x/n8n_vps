a4fef1f1d6da61410d1057b51b34731395f1d360
Shine007x <90572303+God109@users.noreply.github.com>
2025-04-29 09:23:59 +0630

    Add files via upload

--- n8n_install.sh
+++ n8n_install.sh
@@ -14,15 +14,13 @@ echo "✅ Docker installation completed!"
 echo "📂 Creating n8n data volume..."
 cd ~
 mkdir n8n_data
-mkdir n8n_temp
-sudo chown -R 1000:1000 n8n_data n8n_temp
-sudo chmod -R 755 n8n_data n8n_temp
-echo "✅ n8n data volume and n8n_temp directory are ready!"
+sudo chown -R 1000:1000 n8n_data
+sudo chmod -R 755 n8n_data
+echo "✅ n8n data volume is ready!"
 
 # Docker Compose Setup
 echo "🐳 Setting up Docker Compose..."
-wget https://raw.githubusercontent.com/God109/n8n_vps/refs/heads/main/compose.yaml -O compose.yaml
-
+wget https://raw.githubusercontent.com/zero2launch/n8n_vps/refs/heads/main/compose.yaml -O compose.yaml
 export EXTERNAL_IP=http://"$(hostname -I | cut -f1 -d' ')"
-sudo -E docker-compose up -d
+sudo -E docker compose up -d
 echo "🎉 Installation complete! Access your service at: $EXTERNAL_IP"
