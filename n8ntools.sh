5e999a62be3f90ecc67d616ff5edd5024ed72808
Shine007x <90572303+God109@users.noreply.github.com>
2025-04-29 11:15:42 +0630

    Update n8ntools.sh

--- n8ntools.sh
+++ n8ntools.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+set -e
 
 # Variables
 CONTAINER_NAME="n8n_container"
 
 # Install system packages
-docker exec -it -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc python3-dev musl-dev curl ffmpeg
+docker exec -i -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc python3-dev musl-dev curl ffmpeg
 
 # Install yt-dlp
-docker exec -it -u root "$CONTAINER_NAME" apk add yt-dlp
+docker exec -i -u root "$CONTAINER_NAME" apk add yt-dlp
 
 # Install Telethon using pip
-docker exec -it -u root "$CONTAINER_NAME" pip install telethon
+docker exec -i -u root "$CONTAINER_NAME" pip install telethon
 
-echo "🎉 Tools installation completed!"
+# Done
+echo "🎉 Installation completed successfully!"
