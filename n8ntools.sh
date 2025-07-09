e0b2fa66c1aa030ae591f64199135ba129376da4
Shine007x <90572303+Shine007x@users.noreply.github.com>
2025-07-09 22:48:10 +0630

    Update n8ntools.sh

--- n8ntools.sh
+++ n8ntools.sh
@@ -10,10 +10,8 @@ docker exec -i -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc py
 # Install yt-dlp
 docker exec -i -u root "$CONTAINER_NAME" apk add yt-dlp
 
-# Install Telethon and Whisper (with break-system-packages)
-docker exec -i -u root "$CONTAINER_NAME" pip install --break-system-packages torch torchvision torchaudio
-
-docker exec -i -u root "$CONTAINER_NAME" pip install --break-system-packages openai-whisper telethon
+# Install Telethon (with break-system-packages)
+docker exec -i -u root "$CONTAINER_NAME" pip install --break-system-packages telethon
 
 # Done
 echo "🎉 Tools installation completed!"
