7ad14114f206663b84bef7be002ed474bca44465
Shine007x <90572303+Shine007x@users.noreply.github.com>
2025-07-09 22:29:21 +0630

    Update n8ntools.sh

--- n8ntools.sh
+++ n8ntools.sh
@@ -4,13 +4,14 @@ set -e
 # Variables
 CONTAINER_NAME="n8n_container"
 
-# Update apk
-docker exec -i -u root "$CONTAINER_NAME" apk update
+# Install ffmpeg
+docker exec -i -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc python3-dev musl-dev curl ffmpeg
 
-# Install python3, pip, gcc, musl-dev, curl, ffmpeg, yt-dlp
-docker exec -i -u root "$CONTAINER_NAME" apk add --no-cache python3 py3-pip gcc python3-dev musl-dev curl ffmpeg yt-dlp
+# Install yt-dlp
+docker exec -i -u root "$CONTAINER_NAME" apk add yt-dlp
 
-# Install Telethon and Whisper Python packages with break-system-packages
-docker exec -i -u root "$CONTAINER_NAME" pip3 install --break-system-packages telethon openai-whisper
+# Install Telethon and Whisper (with break-system-packages)
+docker exec -i -u root "$CONTAINER_NAME" pip install --break-system-packages telethon openai-whisper
 
+# Done
 echo "🎉 Tools installation completed!"
