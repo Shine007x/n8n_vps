8c2025bbc46bb6211ad9ab0414acb9a25f1c7363
Shine007x <90572303+Shine007x@users.noreply.github.com>
2025-11-01 00:21:04 +0630

    Update n8ntools.sh

--- n8ntools.sh
+++ n8ntools.sh
@@ -11,7 +11,7 @@ docker exec -i -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc py
 docker exec -i -u root "$CONTAINER_NAME" apk add yt-dlp
 
 # Install cuffi
-docker exec -it -u root n8n_local pip install "yt-dlp[default,curl-cffi]" --break-system-packages
+docker exec -it -u root "$CONTAINER_NAME" pip install "yt-dlp[default,curl-cffi]" --break-system-packages
 
 # Install Telethon (with break-system-packages)
 docker exec -i -u root "$CONTAINER_NAME" pip install --break-system-packages telethon
