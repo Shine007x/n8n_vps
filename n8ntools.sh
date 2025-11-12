adc8351d6fec786ba68147c06c2416a428de001c
Shine007x <90572303+Shine007x@users.noreply.github.com>
2025-11-13 04:54:14 +0630

    Update n8ntools.sh

--- n8ntools.sh
+++ n8ntools.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Variables
-CONTAINER_NAME="n8n_container"
+CONTAINER_NAME="n8n-usk8gwkoco4kkg8gs8cg80ow"
 
 # Install ffmpeg
 docker exec -i -u root "$CONTAINER_NAME" apk add --update python3 py3-pip gcc python3-dev musl-dev curl ffmpeg
